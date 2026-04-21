81cb06ac8444c1b017f7dc19fefa6e5ea3a1485e
h.rumiantsev@ncr.gov.ua <h.rumiantsev@ncr.gov.ua>
2026-04-21 08:08:39 +0000

    Add old and new type of create sg

---
+++ security_group/sg_old.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  assign_generated_ipv6_cidr_block = true
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.myvpc.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.myvpc.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
