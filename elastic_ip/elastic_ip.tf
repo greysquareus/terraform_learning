0594c7e2920f5adc1c19fc5023e77c157ba4ff9a
h.rumiantsev@ncr.gov.ua <h.rumiantsev@ncr.gov.ua>
2026-04-21 16:27:20 +0000

    elastic_ip

--- elastic_ip/elastic_ip.tf
+++ elastic_ip/elastic_ip.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-##latest ubuntu for us-east-2
+##latest ubuntu for us-east-2------------------
 data "aws_ami" "latest_ubuntu" {
   owners      = ["099720109477"]
   most_recent = true
@@ -11,11 +11,49 @@ data "aws_ami" "latest_ubuntu" {
     values = ["ubuntu/images/hvm-ssd-*/ubuntu-noble-24.04-amd64-server-*"]
   }
 }
+####-------------
+
+resource "aws_vpc" "db" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "database"
+  }
+}
+
+
+resource "aws_security_group" "sg_database" {
+  name        = "db_acess"
+  description = "Allow all trafic to db"
+  vpc_id      = aws_vpc.db.id
+
+  tags = {
+    Name = "allow_db"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "${aws_eip.lb.public_ip}/32"
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.sg_database.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 
 #aws instance
 resource "aws_instance" "web" {
   ami = data.aws_ami.latest_ubuntu.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = ""
+
 }
 
 #static ip
